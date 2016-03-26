5e0fde6c99fb1e3f2af0569d038b99272f989dd1
thomasyu888 <thomasyu888@gmail.com>
2016-03-25 21:18:40 -0700

    Add in json input

--- runExample.sh
+++ runExample.sh
@@ -2,4 +2,4 @@
 cd test_tophatCWL &&\
 wget http://ccb.jhu.edu/software/tophat/downloads/test_data.tar.gz &&\
 tar -zxvf test_data.tar.gz && \
-cwl-runner test-workflow.cwl
+cwl-runner test-workflow.cwl test-input.json
